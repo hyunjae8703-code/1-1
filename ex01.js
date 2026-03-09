521cfafa80dd6fe14cab7eebe838462c4afec79d
hyunjae8703-code <hyunjae8703@gmail.com>
2026-03-09 15:46:41 +0900

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,30 @@
+//a.py :파이썬
+//a.c :C언어
+//a.java : java
+//command + s :
+//command + q : (터미널)프롬포트 종료
+//exo1.js
+//html,css, javasciprt
+// javasciprt - node - react 리엑트 네이티브 플로터, next.js nest.js
+
+//ls : 리스트 목록
+//mkdir : 디렉터리를 생성
+//pwd : 
+
+
+//node 설치
+//VScode 설치
+
+
+
+const a = 10;
+console.log(a); // print
+const b = 20;
+console.log(b);
+console.log(a + b);
+const c = 91;
+console.log(c); // print
+const d = 88;
+console.log(d);
+console.log(c + d);
+console.log(a + b + c + d);
